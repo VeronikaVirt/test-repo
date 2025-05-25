2385c71c569dd8333461fe491e51339f2915a483
VeronikaVirt <nikaanriivna05@gmail.com>
2025-05-25 17:29:33 +0300

    main.js fixed, new expecter results was added

--- main.js
+++ main.js
@@ -6,6 +6,3 @@ export function multiply(a, b){
 export function sumDigits(a,b) {
     return a + b;
 }
-
-
-console.log(multiply(2,3));
--- main.test.js
+++ main.test.js
@@ -2,6 +2,8 @@ import { multiply, sumDigits } from './main.js';
 
 test('multiply 2 * 3 = 6', () => {
   expect(multiply(2, 3)).toBe(6);
+  expect(multiply(3, 9)).toBe(27);
+  expect(multiply(0, 3)).toBe(0);
 });
 
 test('sumDigits 2 + 3 = 5', () => {
